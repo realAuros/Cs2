655464dd012e0a73e9e7182f3c2f83155e89691b
Auros <massiveb7@gmail.com>
2026-02-24 22:27:17 +0100

    Add files via upload

--- scenesystem_dll.cs
+++ scenesystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-10 14:45:30.515821700 UTC
+// 2026-02-22 00:58:10.373456700 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: scenesystem.dll
